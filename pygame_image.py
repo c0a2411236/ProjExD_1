072b7010cac6cd7402ba1d68778b3cbcbfe5c0ac
Sai Shinoki <c0a2411236@edu.teu.ac.jp>
2025-06-10 16:03:56 +0900

    練習問題終了!

--- pygame_image.py
+++ pygame_image.py
@@ -12,8 +12,14 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
     bg_img3 = bg_img
+
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
+
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300, 200
+    
+
     tmr = 0
 
 
@@ -21,13 +27,24 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0,+1))
+        elif key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((+1,0))
+
+
         screen.blit(bg_img, [-tmr, 0])
 
-        screen.blit(kt_img, [300, 200])
+        screen.blit(kt_img,kt_rct)
+        #screen.blit(kt_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(400)
+        clock.tick(200)
             
         if (tmr >= 800):
             screen.blit(bg_img2, [-tmr+1600, 0])
